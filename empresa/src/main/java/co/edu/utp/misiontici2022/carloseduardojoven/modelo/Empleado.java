596cafc07a40d417f03a2b3239dd2b889b186b34
cejoven-deb <carlosvivax77@hotmail.com>
2022-07-15 19:50:06 -0500

    empresa

---
+++ empresa/src/main/java/co/edu/utp/misiontici2022/carloseduardojoven/modelo/Empleado.java
@@ -0,0 +1,26 @@
+
+ 
+package co.edu.utp.misiontici2022.carloseduardojoven.modelo;
+
+/**
+ *
+ * @author carlos
+ */
+public class Empleado extends Persona {
+    //Atributos
+    
+    private Double  sueldoBruto;
+    
+//Declaración de los mpetodos
+    
+    @Override
+    
+    public void mostrar(){
+        
+    }
+    
+     public void   calcularSalarioNeto(){
+         
+     }
+    
+}
---
+++ empresa/src/main/java/co/edu/utp/misiontici2022/carloseduardojoven/modelo/Persona.java
@@ -0,0 +1,16 @@
+
+package co.edu.utp.misiontici2022.carloseduardojoven.modelo;
+
+/**
+ *
+ * @author carlos
+ */
+public  abstract class Persona {
+    public String nombre;
+    public Integer edad;
+    
+   //TODO: METODO 
+    public abstract void mostrar();
+    
+    
+}
